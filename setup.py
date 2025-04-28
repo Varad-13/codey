0a08531cc101928968819a378a36eaebeffaf065
Varad J <varad.gcs@gmail.com>
2025-04-28 20:00:51 +0530

    chore: update version to 0.1.2 and review dependencies
     - Signed off by Codey

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codey',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(include=['codey', 'codey.*']),
     include_package_data=True,
     install_requires=[
