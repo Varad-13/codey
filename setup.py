dab4d8ad75506c31fd42c3511b6a84abad20aa9f
Varad J <varad.gcs@gmail.com>
2025-08-22 18:20:04 +0530

    chore: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codey',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(include=['codey', 'codey.*']),
     include_package_data=True,
     install_requires=[
